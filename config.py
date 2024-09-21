b4695684c40c7e6169488109db9e7d12fd7036e6
Lovelyavimuwal <108040500+Lovelyavimuwal@users.noreply.github.com>
2024-09-21 22:45:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 BOT_USERNAME = environ.get("BOT_USERNAME", "Filelelebhaibot") # without @
 PORT = environ.get("PORT", "8080")
 #force sub channel information
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1001993473554').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1001993473554 -1002094750016').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 # Clone Info :-
 CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
