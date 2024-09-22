83faab57e5a89afd21d54bb54c61f7702adb79e8
Lovelyavimuwal <108040500+Lovelyavimuwal@users.noreply.github.com>
2024-09-22 22:43:09 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -185,7 +185,7 @@ async def gen_link_batch(bot, message):
                 pass
     with open(f"batchmode_{message.from_user.id}.json", "w+") as out:
         json.dump(outlist, out)
-    post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore. @snap_leaks_2024")
+    post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️Generated for filestore.")
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
     user_id = message.from_user.id
